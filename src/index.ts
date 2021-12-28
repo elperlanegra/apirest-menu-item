9e555d34c27fdbcb778905a9c0a31a6822bb8ed8
Manuel Duarte <carlosd99m@gmail.com>
2021-12-28 02:36:58 -0600

    style: reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
 /**
  * Required modules
  */
-import * as dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 
-dotenv.config();
+dotenv.config()
 
-/**
+/**  
  * App Variables
  */
 
 if (!process.env.PORT) {
-  process.exit(1);
+  process.exit(1)
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
-const app = express();
+const PORT: number = parseInt(process.env.PORT as string, 10)
+const app = express()
 
 /**
  * App Configurations
  */
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
 
 /**
  * Server Activations
  */
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on port ${PORT}`)
 })
